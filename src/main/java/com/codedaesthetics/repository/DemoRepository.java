aa40f7434229f162451f35309454b1a475be6514
Jan Reinsch <reinsch.jan@gmail.com>
2017-03-15 11:29:29 +0100

    added simple sample @Query with inner joins

--- src/main/java/com/codedaesthetics/repository/DemoRepository.java
+++ src/main/java/com/codedaesthetics/repository/DemoRepository.java
@@ -1,6 +1,7 @@
 package com.codedaesthetics.repository;
 
 import com.codedaesthetics.domain.Demo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -11,4 +12,6 @@ import java.util.List;
  */
 public interface DemoRepository extends CrudRepository<Demo, Long> {
     List<Demo> findDemoByName(@Param("name") String name);
+    @Query("SELECT d FROM Demo d INNER JOIN d.participants ps INNER JOIN ps.person p WHERE p.lastName = :lastName")
+    List<Demo> findByPersonLastName(@Param("lastName") String lastName);
 }
