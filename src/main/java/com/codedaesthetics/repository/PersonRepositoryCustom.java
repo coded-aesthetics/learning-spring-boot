72883daf3841773bdd4b8daa0bf2a78364699f3b
Jan Reinsch <reinsch.jan@gmail.com>
2017-03-25 07:10:58 +0100

    added files that are not yet used, for next step -> custom extensions of jpa repositories

--- src/main/java/com/codedaesthetics/repository/PersonRepositoryCustom.java
+++ src/main/java/com/codedaesthetics/repository/PersonRepositoryCustom.java
@@ -1,7 +1,13 @@
 package com.codedaesthetics.repository;
 
+import com.codedaesthetics.domain.Person;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 /**
  * Created by coded-aesthetics on 24.03.17.
  */
 public interface PersonRepositoryCustom {
+    List<Person> findSomeCustomMethod(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName);
 }
--- src/main/java/com/codedaesthetics/repository/PersonRepositoryImpl.java
+++ src/main/java/com/codedaesthetics/repository/PersonRepositoryImpl.java
@@ -1,7 +1,12 @@
 package com.codedaesthetics.repository;
 
+import com.codedaesthetics.controller.PersonController;
 import com.codedaesthetics.domain.Person;
+import com.codedaesthetics.resource.PersonResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +17,16 @@ import java.util.List;
  * Created by coded-aesthetics on 24.03.17.
  */
 public class PersonRepositoryImpl implements PersonRepositoryCustom {
+
+    @Autowired
+    private PersonRepository personRepository;
+
     @Override
-    public List<Person> someCustomMethod(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
-        return null;
+    public List<Person> findSomeCustomMethod(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+        List<Person> first = personRepository.findPersonByFirstName(firstName);
+        List<Person> last = personRepository.findPersonByLastName(lastName);
+        first.addAll(last);
+
+        return first;
     }
 }
